1a8bdea0fe946727ae8283c5a5346db88ddeb783
kaigezhang <zhangkeger@qq.com>
2018-04-13 07:48:52 +0800

    back to old settings

--- backend/scholar/settings.py
+++ backend/scholar/settings.py
@@ -86,18 +86,18 @@ WSGI_APPLICATION = 'scholar.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    },
     # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    #     'NAME':os.environ["POSTGRES_DB"],
-    #     'USER':os.environ["POSTGRES_USER"],
-    #     'PASSWORD':os.environ["POSTGRES_PASSWORD"],
-    #     'HOST': 'postgres',
-    #     'PORT': '',
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     # },
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME':os.environ["POSTGRES_DB"],
+        'USER':os.environ["POSTGRES_USER"],
+        'PASSWORD':os.environ["POSTGRES_PASSWORD"],
+        'HOST': 'postgres',
+        'PORT': '',
+    },
 }
 
 
